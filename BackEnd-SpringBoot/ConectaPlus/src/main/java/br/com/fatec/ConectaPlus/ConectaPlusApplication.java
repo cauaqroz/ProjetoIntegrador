720a4dc0b5fa5635d5c183c3b10ca6da8afff34b
cauaqroz <cauaqroz@gmail.com>
2024-08-14 11:46:49 -0300

    Fet: Criando Estruturação

---
+++ BackEnd-SpringBoot/ConectaPlus/src/main/java/br/com/fatec/ConectaPlus/ConectaPlusApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.ConectaPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaPlusApplication.class, args);
+	}
+
+}
